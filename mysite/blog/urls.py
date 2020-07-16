0fcbc4ededc6b99ebe37b2291bf5d3e9dfc399ca
Mike Wheeler <mikeytwheeler@gmail.com>
2020-07-15 21:09:59 -0700

    first covidaApp2 commit

---
+++ mysite/blog/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path, include
+from .feeds import LatestPostsFeed, AtomSiteNewsFeed
+
+urlpatterns = [
+    path("feed/rss", LatestPostsFeed(), name="post_feed"),
+    path("feed/atom", AtomSiteNewsFeed()),
+    path("", views.PostList.as_view(), name="home"),
+    # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path("<slug:slug>/", views.post_detail, name="post_detail"),
+]
